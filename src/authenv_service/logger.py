a13ddac2affabc547d5fddc13177263352160f5c
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2023-12-17 09:27:41 -0700

    Make File Logger Optional (#45)

--- src/authenv_service/logger.py
+++ src/authenv_service/logger.py
@@ -10,26 +10,12 @@ from constants import REPO_HOME
 
 class Logger:
     def __init__(self, logger: logging.Logger, module_name: str):
-        log_file_location = REPO_HOME + "/logs/authenv-service/authenv-service.log"
         self.logger = logger
         self.logger.setLevel(logging.INFO)
 
         # console logger
         self.stream_handler = logging.StreamHandler(sys.stdout)
-        self.stream_handler.setLevel(logging.ERROR)
-
-        # file logger
-        log_dir = os.path.dirname(log_file_location)
-        os.makedirs(log_dir, exist_ok=True)
-        self.file_handler = TimedRotatingFileHandler(
-            log_file_location,
-            when="midnight",
-            interval=1,  # Daily rotation
-            backupCount=14,  # Keep logs for 14 days
-            encoding="utf-8",
-            delay=True,
-        )
-        self.file_handler.setLevel(logging.INFO)
+        self.stream_handler.setLevel(logging.INFO)
 
         self.formatter = logging.Formatter(
             f"[%(asctime)s] [authenv-service] [{module_name}] "
@@ -42,10 +28,27 @@ class Logger:
 
         self.formatter.converter = converter
         self.stream_handler.setFormatter(self.formatter)
-        self.file_handler.setFormatter(self.formatter)
 
         self.logger.addHandler(self.stream_handler)
-        self.logger.addHandler(self.file_handler)
+
+        # file logger
+        if REPO_HOME is not None:
+            log_file_location = REPO_HOME + "/logs/authenv-service/authenv-service.log"
+            log_dir = os.path.dirname(log_file_location)
+            os.makedirs(log_dir, exist_ok=True)
+            self.file_handler = TimedRotatingFileHandler(
+                log_file_location,
+                when="midnight",
+                interval=1,  # Daily rotation
+                backupCount=14,  # Keep logs for 14 days
+                encoding="utf-8",
+                delay=True,
+            )
+            self.file_handler.setLevel(logging.INFO)
+            # if file logger is present, set console logger level at ERROR
+            self.stream_handler.setLevel(logging.ERROR)
+            self.file_handler.setFormatter(self.formatter)
+            self.logger.addHandler(self.file_handler)
 
     def debug(self, msg):
         self.logger.debug(msg)
@@ -59,4 +62,6 @@ class Logger:
     def set_level(self, level):
         self.logger.setLevel(level)
         self.stream_handler.setLevel(level)
-        self.file_handler.setLevel(level)
+
+        if self.file_handler is not None:
+            self.file_handler.setLevel(level)
